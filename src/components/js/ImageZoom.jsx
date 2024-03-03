3eb08b4a8dd469076bc0e50342ad2d8e2d38cfb3
andy duong <andyduong510@gmail.com>
2024-03-03 11:49:09 -0800

    image header

---
+++ src/components/js/ImageZoom.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import mediumZoom from 'medium-zoom';
+
+export function ImageZoom({ options, ...props }) {
+  const zoomOptions = {
+    background: '#111111',
+    ...options
+  };
+
+  const zoomRef = useRef(null);
+
+  function getZoom() {
+    if (zoomRef.current === null) {
+      zoomRef.current = mediumZoom(zoomOptions);
+    }
+
+    return zoomRef.current;
+  }
+
+  function attachZoom(image) {
+    const zoom = getZoom();
+
+    if (image) {
+      zoom.attach(image);
+    } else {
+      zoom.detach();
+    }
+  }
+
+  return <img {...props} ref={attachZoom} />;
+}

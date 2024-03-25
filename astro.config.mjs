fe95859671cc9500581b8190a23e23d9807e7970
andy duong <andyduong510@gmail.com>
2024-03-25 16:26:59 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -9,5 +9,6 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: "passthrough",
   }),
-  integrations: [react()],
+  site: "https://andyduong.work",
+  integrations: [react(), sitemap()],
 });

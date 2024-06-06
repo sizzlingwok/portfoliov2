e9c69752ac68a29beab443a7f1d08c99e809043b
andy duong <andyduong510@gmail.com>
2024-06-06 15:52:52 -0700

    home page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface Window {
-  gsap: typeof import("gsap");
-  ScrollTrigger: typeof import("gsap/ScrollTrigger");
+  gsap: typeof import("gsap").gsap;
+  ScrollTrigger: typeof import("gsap/ScrollTrigger").ScrollTrigger;
 }

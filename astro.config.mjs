57174ef340174519090707197b7978a63734b286
andy duong <andyduong510@gmail.com>
2024-06-10 13:39:57 -0700

    astro font optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,13 @@ export default defineConfig({
   output: "server",
   adapter: cloudflare({
     imageService: "passthrough",
+    vite: {
+      ssr: {
+        external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map(
+          (i) => `node:${i}`
+        ),
+      },
+    },
   }),
   site: "https://andyduong.work",
   integrations: [react(), sitemap()],

160243654541c30d9ccd430ed5270a5204f19dee
andy duong <andyduong510@gmail.com>
2024-06-10 14:19:20 -0700

    nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
     imageService: "passthrough",
     vite: {
       ssr: {
-        external: ["node:buffer"],
+        external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map(
+          (i) => `node:${i}`
+        ),
       },
     },
   }),

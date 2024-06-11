66154170afd0511c796b8a6c9de5dfc57080e85d
andy duong <andyduong510@gmail.com>
2024-06-10 19:37:15 -0700

    nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,26 @@ export default defineConfig({
   output: "server",
   adapter: cloudflare({
     imageService: "passthrough",
-    vite: {
-      ssr: {
-        external: ["buffer", "path", "fs", "os", "crypto", "async_hooks"].map(
-          (i) => `node:${i}`
-        ),
+  }),
+  vite: {
+    ssr: {
+      external: [
+        "node:buffer",
+        "node:path",
+        "node:fs",
+        "node:os",
+        "node:crypto",
+      ],
+    },
+    resolve: {
+      alias: {
+        path: "node:path",
+        fs: "node:fs",
+        os: "node:os",
+        crypto: "node:crypto",
       },
     },
-  }),
+  },
   site: "https://andyduong.work",
   integrations: [react(), sitemap()],
 });
